9d2f42c0998de641beffd703e3e90a77967e2fba
Huzaifa Ahmed <huzaifasaleem098@gmail.com>
2025-06-09 18:30:08 +0500

    update form

--- client/src/components/auth/SignupForm.tsx
+++ client/src/components/auth/SignupForm.tsx
@@ -3,31 +3,72 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
-import githubLogo from "@/assets/githubLogo.svg";
+// import githubLogo from "@/assets/githubLogo.svg";
+// import githubLogo from "@/assets/githubLogo.svg";
+import { SignupSchema, type SignupSchemaType } from "@/schema";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "../ui/form";
 
 export function SignupForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
+  const form = useForm<SignupSchemaType>({
+    resolver: zodResolver(SignupSchema),
+    defaultValues: {
+      username: "",
+      fullName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<SignupSchemaType> = (data) => {};
+
   return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
+    <Form {...form}>
+    <form className={cn("flex flex-col gap-6", className)} {...props} onSubmit={form.handleSubmit(onSubmit)}>
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-2xl font-bold">Sign Up New Account </h1>
         <p className="text-muted-foreground text-sm text-balance">
           Enter your details below to create a new account
         </p>
       </div>
-      <div className="grid gap-6">
-        {/* FullName field */}
-        <div className="grid gap-3">
-          <Label htmlFor="email">Full Name</Label>
-          <Input id="fullName" type="text" placeholder="Huzaifa Ahmed" required />
-        </div>
 
+      <FormField 
+      control={form.control}
+      name="username"
+      render={({ field}) => (
+        <FormItem>
+          <FormLabel>Username</FormLabel>
+          <FormControl>
+            <Input placeholder="0xHuzaifa" {...field} />
+          </FormControl>
+          <FormDescription>
+            {form.formState.errors.username}
+          </FormDescription>
+        </FormItem>
+      )} 
+      />
+
+      <div className="grid gap-6">
         {/* Username field */}
         <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
-          <Input id="username" type="text" placeholder="0xHuzaifa" required />
+          <Label htmlFor="username">Username</Label>
+          <Input id="Username" type="text" placeholder="0xHuzaifa" required />
+        </div>
+
+        {/* FullName field */}
+        <div className="grid gap-3">
+          <Label htmlFor="fullName">Full Name</Label>
+          <Input
+            id="fullName"
+            type="text"
+            placeholder="Huzaifa Ahmed"
+            required
+          />
         </div>
 
         {/* Email field */}
@@ -36,28 +77,30 @@ export function SignupForm({
           <Input id="email" type="email" placeholder="m@example.com" required />
         </div>
 
-        
-        <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
-        </div>
-
         {/* Password field */}
         <div className="grid gap-3">
-          <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
-            <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Forgot your password?
-            </a>
-          </div>
-          <Input id="password" type="password" required />
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            type="password"
+            placeholder="********"
+            required
+          />
+        </div>
+
+        {/* Confirm Password field */}
+        <div className="grid gap-3">
+          <Label htmlFor="confirmPassword">Confirm Password</Label>
+          <Input
+            id="confirmPassword"
+            type="password"
+            placeholder="********"
+            required
+          />
         </div>
 
         <Button type="submit" className="w-full">
-          Login
+          SignUp
         </Button>
 
         {/* OAuth section */}
@@ -78,5 +121,6 @@ export function SignupForm({
         </Link>
       </div>
     </form>
+    </Form>
   );
 }
---
+++ client/src/schema/index.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+export const SignupSchema = z
+  .object({
+    username: z.string().min(3, "Username must be at least 3 characters long"),
+    fullName: z.string().min(5, "Full Name must be at least 5 characters long"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+    confirmPassword: z
+      .string()
+      .min(6, "Confirm Password must be at least 6 characters long"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export type SignupSchemaType = z.infer<typeof SignupSchema>;
