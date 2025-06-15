f87c64bf13f888ae1fb1e46cf52842083f8a5759
Huzaifa Ahmed <huzaifasaleem098@gmail.com>
2025-06-15 19:13:04 +0500

    uninstall redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,15 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import store from "@/store";
-import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
