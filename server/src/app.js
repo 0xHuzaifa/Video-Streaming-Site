6827ce43ed7843b8bccf29c3fd9f128ce88a3e58
Huzaifa Ahmed <huzaifasaleem098@gmail.com>
2025-06-19 13:53:35 +0500

    add cors configuration

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,12 @@
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import express from "express";
 import errorHandler from "./middlewares/errorHandler.js";
+import corsConfig from "./configs/cors.config.js";
 
 const app = express();
 
 // middlewares
-app.use(cors()); // Enable CORS
+app.use(corsConfig()); // Enable CORS
 app.use(express.json({ limit: "16kb" })); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); // Parse URL-encoded bodies
 app.use(express.static("public")); // Serve static files from the public directory
