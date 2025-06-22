aa7d43425adc323fa73d6d36a79e93c70e0e50a8
Huzaifa Ahmed <huzaifasaleem098@gmail.com>
2025-06-22 18:31:02 +0500

    import & add toaster from sonner

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
 import LoginPage from "@/pages/auth-pages/Login";
 import SignupPage from "@/pages/auth-pages/Signup";
 import { Routes, Route } from "react-router-dom";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-    </Routes>
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </>
   );
 }
 
