729be02ec1f906a01034501cab06a6d07d0ef147
Huzaifa Ahmed <huzaifasaleem098@gmail.com>
2025-06-27 23:29:02 +0500

    add authResponse types

--- client/src/lib/types/authTypes.ts
+++ client/src/lib/types/authTypes.ts
@@ -1,8 +1,34 @@
-interface AuthResponse {
+interface BaseAuthResponse {
   statusCode: number;
   message: string;
-  data?: object;
+  success: boolean;
 }
 
+interface UnVerifiedUserAuthResponse extends BaseAuthResponse {
+  success: true;
+}
+
+interface VerifiedUserAuthResponse extends BaseAuthResponse {
+  data: {
+    loggedInUser: {
+      id: string;
+      fullName: string;
+      username: string;
+      email: string;
+      isVerified: boolean;
+    };
+  };
+}
+
+interface ErrorAuthResponse extends BaseAuthResponse {
+  success: false;
+  errors: [];
+  stack?: string;
+}
+
+type AuthResponse =
+  | UnVerifiedUserAuthResponse
+  | VerifiedUserAuthResponse
+  | ErrorAuthResponse;
 
 export type { AuthResponse };
