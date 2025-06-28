e78da869034872fa6edf6685f6fd4cd5f5993406
Huzaifa Ahmed <huzaifasaleem098@gmail.com>
2025-06-28 20:46:59 +0500

    create useContext hook

---
+++ client/src/hooks/UseAppContext.ts
@@ -0,0 +1,11 @@
+// useAppContext.ts
+import { useContext } from "react";
+import { AppContext } from "@/context";
+
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("useAppContext must be used within an AppProvider");
+  }
+  return context;
+};
