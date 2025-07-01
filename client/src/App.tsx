f9db217dba0d1ebd6754c60f06eae7928f47c23f
Huzaifa Ahmed <huzaifasaleem098@gmail.com>
2025-07-01 12:00:12 +0500

    define routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,25 @@
+// pages
 import LoginPage from "@/pages/auth-pages/Login";
 import SignupPage from "@/pages/auth-pages/Signup";
+import Home from "@/pages/Home";
+
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "./components/ui/sonner";
+import { ProtectedRoute, GuestOnlyRoute } from "./auth/ProtectedRoute";
 
 function App() {
   return (
     <>
       <Toaster />
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route element={<GuestOnlyRoute />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Route>
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </>
   );
